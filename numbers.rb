643953bfdddc5e4b56cd866d59def8030e552a45
Kimberly N <k.narine@yahoo.com>
2013-06-04 13:07:10 -0400

    numbers tutorial

---
+++ numbers.rb
@@ -0,0 +1,13 @@
+puts "how many hours are in a year? \n"
+puts 24 * 365
+
+puts "how many minutes are in a decade? \n"
+puts 60 * 24 * 365 * 10
+
+puts "how many seconds old are you? \n"
+puts 60 * 60 * 24 * 365 * 22
+
+puts "If I am 1031 million seconds old, how old am I? \n"
+age_in_sec = 1031000000
+age_in_years = age_in_sec / (60 * 60 * 24 * 365)
+puts age_in_years
