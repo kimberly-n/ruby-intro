db6ff00e41d3066fb6f96cb9a33c62a6cc9277be
Kimberly N <k.narine@yahoo.com>
2013-06-05 23:02:14 -0400

    reverse string

--- reverseString.rb
+++ reverseString.rb
@@ -1,11 +1,11 @@
 #Use a stack to reverse a string? You will need String#split and Array#join.
 
 def reverseStr (arr2rev)
-  arr2 = ''
+  arr2 = []
   while arr2rev.length > 0
     arr2 << arr2rev.pop
   end
-  arr2
+  arr2.join
 end
 
 puts 'Enter asome text'
